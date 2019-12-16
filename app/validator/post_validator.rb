3a32351dfd455e7653c2eb8edcdde56c8717fb82
figueroar <raul.a.figueroa@hotmail.com>
2019-12-16 10:30:31 -0500

    Done.

---
+++ app/validator/post_validator.rb
@@ -0,0 +1,21 @@
+#class PostValidator < ActiveModel::Validator
+  #def validate(post)
+  #  
+    #binding.pry
+    #if post.id = nil
+      #post.save
+    #end
+
+    #unless post.title != nil
+      #post.errors[:title] << "Post is invalid without a title"
+    #end
+
+    #unless post.category != nil
+      #post.errors[:category] << "Empty"
+    #end
+
+    #unless post.content = nil
+      #post.errors[:category] << "invalid category"
+    #end
+  e#nd
+#end
