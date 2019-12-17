4da8d6bd1a7f9dc965fde24ed087ea82e8bb1af1
Jesse <jesse.lee.smith@gmail.com>
2019-12-17 13:30:10 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, length: { minimum: 1 }
+  validates :category, presence: true, inclusion: { in: %w[Fiction Non-Fiction] }
+  validates :content, length: { minimum: 100 }
 end
