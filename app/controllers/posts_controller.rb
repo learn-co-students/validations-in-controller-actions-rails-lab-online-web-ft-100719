142ce1b5726c933167351baf4dab8fb3d0886a62
Victoria Hale <vic.hale74@gmail.com>
2019-12-17 13:10:00 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,12 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
+    if !@post.valid?
+      render :edit
+    else
+
     redirect_to post_path(@post)
+    end
   end
 
   private
